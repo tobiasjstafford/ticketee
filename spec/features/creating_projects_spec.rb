8cbe62fa06020b2b198bf8341cdc8aa5bfcf8e84
Toby Stafford <tjs@tobystafford.com>
2013-06-18 21:46:17 -0400

    Adds name validation for project

--- spec/features/creating_projects_spec.rb
+++ spec/features/creating_projects_spec.rb
@@ -1,11 +1,13 @@
 require 'spec_helper'
 
 feature 'Creating Projects' do
-  scenario 'can create a project' do
+  before do
     visit '/'
 
     click_link 'New Project'
+  end
 
+  scenario 'can create a project' do
     fill_in 'Name', with: 'Textmate 2'
     fill_in 'Description', with: 'A text exitor for OS X'
     click_button 'Create Project'
@@ -18,6 +20,13 @@ feature 'Creating Projects' do
 
     title = 'Textmate 2 - Projects - Ticketee'
     expect(find('title').native.text).to have_content(title)
+  end
+
+  scenario 'can not create an invalid project' do
+    click_button 'Create Project'
+
+    expect(page).to have_content('Project has not been created')
+    expect(page).to have_content("Name can't be blank")
   end
 end
 
