0c0082307b077001f2b2bcec9e9ed6305d11708f
Toby Stafford <tjs@tobystafford.com>
2013-06-24 20:51:15 -0400

    Adds pending spec for user profiles.

---
+++ spec/features/user_profile_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+feature 'User Profiles' do
+  scenario 'viewing profile' do
+    pending 'todo: make a test for successfully viewing a user profile'
+  end
+end
+
+feature 'Editing Users' do
+  scenario 'Updating a user' do
+    pending 'todo: make a test for successfully editing a user email or something'
+  end
+end
+
