cab2b186c9464138e317b9968c3637d3eaab8655
Toby Stafford <tobiasjstafford@gmail.com>
2013-06-24 20:04:25 -0400

    Implements a user model.

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,46 @@
+require 'spec_helper'
+
+describe User do
+  describe "passwords" do
+    it "requires password confirmation" do
+      u = User.new(name: 'toby')
+
+      u.save
+      expect(u).to_not be_valid
+
+      u.password = 'password'
+      u.password_confirmation = ''
+
+      u.save
+      expect(u).to_not be_valid
+
+      u.password_confirmation = 'password'
+
+      u.save
+      expect(u).to be_valid
+    end
+
+    it "needs password and password confirmation to match" do
+      u = User.new(name: 'toby',
+                   password: 'password',
+                   password_confirmation: 'password2')
+
+      u.save
+      expect(u).to_not be_valid
+    end
+  end
+
+  describe "authentication" do
+    let(:user) { User.create(name: 'toby',
+                             password: 'password',
+                             password_confirmation: 'password') }
+
+    it "authenticates with a valid password" do
+      expect(user.authenticate("password")).to be
+    end
+
+    it "does not authenticate with an invalid password" do
+      expect(user.authenticate("password2")).to_not be
+    end
+  end
+end
