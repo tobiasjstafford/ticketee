1020ec00dfd8e4850a205fb3b652792e034bb778
Toby Stafford <tjs@tobystafford.com>
2013-06-30 10:00:47 -0400

    Allows users to be created as admins.

--- spec/features/admin/creating_users_spec.rb
+++ spec/features/admin/creating_users_spec.rb
@@ -12,13 +12,26 @@ feature 'Creating users' do
     click_link 'New User'
   end
 
-  scenario 'Creating a new user as an admin' do
+  scenario 'Creating a normal user' do
     fill_in 'Email', with: 'test@example.com'
     fill_in 'user[password]', with: 'password'
     fill_in 'Password confirmation', with: 'password'
     click_button 'Create User'
 
     expect(page).to have_content 'User has been created'
+  end
+
+  scenario 'Creating an admin user' do
+    fill_in 'Email', with: 'admin@example.com'
+    fill_in 'user[password]', with: 'password'
+    fill_in 'Password confirmation', with: 'password'
+    check 'Is an admin?'
+    click_button 'Create User'
+
+    expect(page).to have_content 'User has been created'
+    within '#users' do
+      expect(page).to have_content 'admin@example.com (Admin)'
+    end
   end
 end
 
